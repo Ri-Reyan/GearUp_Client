151f90c7f2f21647a4f63de42a3e66bfb94e01b1
Ri-Reyan <rireyan50@gmail.com>
2026-08-01 01:44:20 +0600

    add signup functionality

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpPage from "../_components/SignUpPage";
+
+const Register = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center p-4">
+      <SignUpPage />
+    </div>
+  );
+};
+
+export default Register;
